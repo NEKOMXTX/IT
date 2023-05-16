73614dcc7fe7bed156a5f98bcb1612459321672b
nekomxtx <pockervlad@gmail.com>
2023-05-16 15:01:42 +0300

    added like count +1

--- zadanie1v2/scripts.js
+++ zadanie1v2/scripts.js
@@ -178,10 +178,26 @@ function disableResetButton () {
   document.getElementById("resetButton").disabled = true;
 }
 
+// Выбираем все элементы с классом like-btn
+const likes = document.querySelectorAll('.like');
 
+// В каждом элементе выбираем плюс и минус. Навешиваем на событие клик функцию render()
+likes.forEach(like => {
+  const plus = like.querySelector('.like-btn');
+  const minus = like.querySelector('.like-btn');
+  const counter_element = like.querySelector('.like-count');
+  
+  let counter = 0;
+  
+  plus.addEventListener('click', () => {
+    render(++counter, counter_element);
+  });
+  
+  
+});
 
-
-
+// Функция обновляет текст
+const render = (counter, counter_element) => counter_element.innerText = counter;
 
         // Для проверки по сабмиту
 // Обработчик события для формы
